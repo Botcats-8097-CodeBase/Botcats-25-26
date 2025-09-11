434c8262c64236b883401239c0137f2b3017d25f
Towster <towster.business@gmail.com>
2025-09-10 19:48:14 -0700

    ALL

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class RobotConstants {
+    public static String leftBackDiveName = "leftBack";
+    public static String leftFrontDiveName = "leftFront";
+    public static String rightBackDiveName = "rightBack";
+    public static String rightFrontDiveName = "rightFront";
+    public static DcMotorSimple.Direction leftBackDiveDirection = DcMotorSimple.Direction.REVERSE;
+    public static DcMotorSimple.Direction leftFrontDiveDirection = DcMotorSimple.Direction.REVERSE;
+    public static DcMotorSimple.Direction rightBackDiveDirection = DcMotorSimple.Direction.FORWARD;
+    public static DcMotorSimple.Direction rightFrontDiveDirection = DcMotorSimple.Direction.FORWARD;
+}

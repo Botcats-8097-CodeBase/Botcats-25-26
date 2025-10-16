b408bc18fc87c9e9211d00916a43c60a77dbb722
SuryaSRajkumar <surya.s.rajkumar@gmail.com>
2025-10-15 19:43:08 -0700

    g

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomizableTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomizableTester.java
@@ -42,10 +42,14 @@ public class CustomizableTester extends LinearOpMode {
 
     public static double motorC0Power = 0.0;
     public static double motorC1Power = 0.0;
+    public static double motorC2Power = 0.0;
+    public static double motorC3Power = 0.0;
 
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor motorC0 = null;
     private DcMotor motorC1 = null;
+    private DcMotor motorC2 = null;
+    private DcMotor motorC3 = null;
 
     @Override
     public void runOpMode() {
@@ -54,15 +58,23 @@ public class CustomizableTester extends LinearOpMode {
 
         motorC0 = hardwareMap.get(DcMotor.class, "motorC0");
         motorC1 = hardwareMap.get(DcMotor.class, "motorC1");
+        motorC2 = hardwareMap.get(DcMotor.class, "motorC2");
+        motorC3 = hardwareMap.get(DcMotor.class, "motorC2");
 
         motorC0.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorC1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorC2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorC3.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         motorC0.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorC1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorC2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorC3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         motorC0.setDirection(DcMotor.Direction.FORWARD);
         motorC1.setDirection(DcMotor.Direction.FORWARD);
+        motorC2.setDirection(DcMotor.Direction.FORWARD);
+        motorC3.setDirection(DcMotor.Direction.FORWARD);
 
         waitForStart();
         runtime.reset();
@@ -72,6 +84,10 @@ public class CustomizableTester extends LinearOpMode {
             motorC0.setPower(motorC0Power);
 
             motorC1.setPower(motorC1Power);
+
+            motorC2.setPower(motorC2Power);
+
+            motorC3.setPower(motorC3Power);
         }
     }
 }
